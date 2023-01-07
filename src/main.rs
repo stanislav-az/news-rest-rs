78aecd76d6977f4791511c2dba919d7dc2d5ae4a
stanislav-az <st.zhdanovich@gmail.com>
2023-01-07 21:45:37 +0700

    parse request method, path, headers

--- src/main.rs
+++ src/main.rs
@@ -6,19 +6,5 @@ use news_rest_rs::service::{
 };
 
 fn main() {
-    let settings = LoggerSettings {
-        log_to_file: Some(String::from("logs/journal.log")),
-        log_to_stderr: true,
-        log_level_starting_from: LogLevel::Debug,
-    };
-    let mut logger = Logger::initialize(&settings);
-    logger.log_debug("Logger started");
-    thread::sleep(Duration::from_secs(1));
-    let mut thread_logger = logger.clone();
-    let thread = thread::spawn(move || {
-        thread_logger.log_info("Hello from another threeeaad!");
-        thread::sleep(Duration::from_secs(1));
-    });
-    logger.log_debug("Joining thread");
-    thread.join().unwrap();
+    run_server()
 }
--- src/service/webserver.rs
+++ src/service/webserver.rs
@@ -1,10 +1,9 @@
 use crate::service::thread_pool::ThreadPool;
+use serde::{Deserialize, Serialize};
 use std::{
-    fs,
+    collections::HashMap,
     io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
-    thread,
-    time::Duration,
 };
 
 pub fn run_server() {
@@ -13,6 +12,7 @@ pub fn run_server() {
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
+        println!("Received connection!");
 
         pool.execute(|| {
             handle_connection(stream);
@@ -21,22 +21,74 @@ pub fn run_server() {
 }
 
 fn handle_connection(mut stream: TcpStream) {
-    let buf_reader = BufReader::new(&mut stream);
-    let request_line = buf_reader.lines().next().unwrap().unwrap();
+    let mut buf_reader = BufReader::new(&mut stream);
+    let lines = buf_reader
+        .by_ref()
+        .lines()
+        .map(|e| e.expect("Expected UTF-8 string"));
+    let request: Vec<String> = lines.take_while(|line| !line.is_empty()).collect();
 
-    let (status_line, filename) = match &request_line[..] {
-        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "static/main_page.html"),
-        "GET /sleep HTTP/1.1" => {
-            thread::sleep(Duration::from_secs(5));
-            ("HTTP/1.1 200 OK", "static/main_page.html")
-        }
-        _ => ("HTTP/1.1 404 NOT FOUND", "static/404.html"),
-    };
+    println!("New request: {:#?}", &request);
+    let parsed_req = parse_request(&request);
+    println!("Parsed request: {:#?}", parsed_req);
+    let req_body: Vec<_> = buf_reader.bytes().map(Result::unwrap).take(18).collect();
+    // let x: String = req_body.into();
+    println!("Request body: {:?}", String::from_utf8(req_body));
 
-    let contents = fs::read_to_string(filename).unwrap();
-    let length = contents.len();
-
-    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
-
+    let response = "HTTP/1.1 200 OK\r\n\r\n";
     stream.write_all(response.as_bytes()).unwrap();
+}
+// TODO do not drop connection after single request if Connection: keep-alive
+
+#[derive(Debug, PartialEq, Eq, Serialize, Deserialize)]
+pub struct Request {
+    pub method: Method,
+    pub uri: URI,
+    pub headers: Headers,
+}
+
+#[derive(Debug, PartialEq, Eq, Serialize, Deserialize)]
+pub enum Method {
+    GET,
+    POST,
+    PATCH,
+    DELETE,
+    PUT,
+}
+
+pub type URI = Vec<String>;
+pub type Headers = HashMap<String, String>;
+
+fn parse_request(req_lines: &Vec<String>) -> Option<Request> {
+    let first_line: Vec<_> = req_lines.get(0)?.split(' ').collect();
+    let method: Method = match *first_line.get(0)? {
+        "GET" => Some(Method::GET),
+        "POST" => Some(Method::POST),
+        "PATCH" => Some(Method::PATCH),
+        "DELETE" => Some(Method::DELETE),
+        "PUT" => Some(Method::PUT),
+        _ => None,
+    }?;
+    let uri: URI = first_line.get(1)?.split('/').map(String::from).collect();
+
+    let headers: HashMap<String, String> = req_lines
+        .iter()
+        .skip(1)
+        .map(|h| {
+            let header: Vec<_> = h.split(": ").collect();
+            let key = header.get(0);
+            let value = header.get(1);
+            match (key, value) {
+                (Some(k), Some(v)) => Some((String::from(*k), String::from(*v))),
+                _ => None,
+            }
+        })
+        .flatten()
+        .collect();
+
+    Some(Request {
+        method,
+        uri,
+        headers,
+    })
 }
