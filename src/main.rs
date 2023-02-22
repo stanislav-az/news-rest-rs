2f73aee4b8ee527c12f0ff22a761c6e4de2687c8
stanislav-az <st.zhdanovich@gmail.com>
2023-02-22 14:43:40 +0700

    init Chapter04/extracting_data

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,35 @@
-use std::net::TcpListener;
+use actix_web::{App, HttpServer};
+use actix_service::Service;
 
-fn main() {
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+mod state;
+mod to_do;
+mod json_serialization;
+mod views;
+mod processes;
 
-    for stream in listener.incoming() {
-        let stream = stream.unwrap();
 
-        println!("Connection established!");
-    }
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        let app = App::new()
+            .wrap_fn(|req, srv| {
+                // srv => routing
+                // req => service request
+                if *&req.path().contains("/item/") {
+                    match views::token::process_token(&req) {
+                        Ok(_token) => println!("the token is passable"),
+                        Err(message) => println!("token error: {}", message)
+                    }
+                }
+                let fut = srv.call(req);
+                async {
+                    let result = fut.await?;
+                    Ok(result)
+                }
+            }).configure(views::views_factory);
+        return app
+    })
+        .bind("127.0.0.1:8000")?
+        .run()
+        .await
 }
