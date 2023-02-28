a968d42c87fb17ffed3717780fbda34907629add
stanislav-az <st.zhdanovich@gmail.com>
2023-02-28 14:10:12 +0700

    fallback handler

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,22 @@
 #[tokio::main]
 async fn main() {
     // Build our application with a single route.
-    let app = axum::Router::new().route("/", axum::routing::get(|| async { "Hello, World!" }));
+    let app = axum::Router::new()
+        .fallback(fallback_handler)
+        .route("/", axum::routing::get(|| async { "Hello, World!" }));
 
     // Run our application as a hyper server on http://localhost:3000.
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
         .serve(app.into_make_service())
         .await
         .unwrap();
+}
+
+/// axum handler for any request that fails to match the router routes.
+/// This implementation returns HTTP status code Not Found (404).
+pub async fn fallback_handler(uri: axum::http::Uri) -> impl axum::response::IntoResponse {
+    (
+        axum::http::StatusCode::NOT_FOUND,
+        format!("No route {}", uri),
+    )
 }
