125e447c93ae081396c789ac5dc7299eec44e055
Stanislav Zhdanovich <st.zhdanovich@gmail.com>
2023-02-28 19:39:58 +0700

    Add basic axum server (#1)
    * hello world
    
    * fallback handler
    
    * add most basic server

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,54 @@
-use std::net::TcpListener;
+use axum::http;
+use axum::routing;
+use axum::Json;
+use diesel::insert_into;
+use diesel::prelude::*;
+use news_rest_rs::db::establish_connection;
+use news_rest_rs::news::stories::NewStory;
+use news_rest_rs::news::stories::Story;
+use news_rest_rs::schema::stories;
 
-fn main() {
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+#[tokio::main]
+async fn main() {
+    // Build our application with a single route.
+    let app = axum::Router::new()
+        .fallback(fallback_handler)
+        .route("/api/stories", routing::post(create_story))
+        .route("/api/stories", routing::get(get_stories))
+        .route("/", routing::get(|| async { "Hello, World!" }));
 
-    for stream in listener.incoming() {
-        let stream = stream.unwrap();
+    // Run our application as a hyper server on http://localhost:3000.
+    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
 
-        println!("Connection established!");
-    }
+/// axum handler for any request that fails to match the router routes.
+/// This implementation returns HTTP status code Not Found (404).
+pub async fn fallback_handler(uri: http::Uri) -> impl axum::response::IntoResponse {
+    (http::StatusCode::NOT_FOUND, format!("No route {}", uri))
+}
+
+// TODO spawn a tokio task for db query
+pub async fn get_stories() -> Json<Vec<Story>> {
+    let mut conn = establish_connection();
+
+    let news = stories::table
+        .order(stories::columns::id.asc())
+        .load::<Story>(&mut conn)
+        .unwrap();
+
+    news.into()
+}
+
+pub async fn create_story(story: Json<NewStory>) -> impl axum::response::IntoResponse {
+    let story: NewStory = story.0;
+    let mut conn = establish_connection();
+
+    let _res = insert_into(stories::table)
+        .values(&story)
+        .execute(&mut conn);
+
+    http::StatusCode::CREATED
 }
