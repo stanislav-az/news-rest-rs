1437c1665efe31d84a74aa4c80ce217d9888ee91
stanislav-az <st.zhdanovich@gmail.com>
2023-04-16 16:41:58 +0700

    handle errors

--- src/news/handlers/stories.rs
+++ src/news/handlers/stories.rs
@@ -1,3 +1,6 @@
+use super::internal_error;
+use super::Error;
+use super::Response;
 use crate::db::establish_connection;
 use crate::news::models::NewStory;
 use crate::news::models::Story;
@@ -5,54 +8,61 @@ use crate::schema::stories;
 use crate::schema::stories::dsl::*;
 use axum::extract::Path;
 use axum::http;
-use axum::response::IntoResponse;
 use axum::Json;
 use diesel::delete;
 use diesel::insert_into;
 use diesel::prelude::*;
 use diesel::update;
 
-pub async fn get_stories() -> Json<Vec<Story>> {
+pub async fn get_stories() -> Result<Json<Vec<Story>>, Error> {
     let mut conn = establish_connection();
 
     let news = stories::table
         .filter(is_published.eq(true))
         .order(stories::columns::id.asc())
         .load::<Story>(&mut conn)
-        .unwrap();
+        .map_err(internal_error)?;
 
-    news.into()
+    Ok(news.into())
 }
 
-pub async fn create_story(story: Json<NewStory>) -> impl IntoResponse {
+pub async fn create_story(story: Json<NewStory>) -> Response {
     let story: NewStory = story.0;
     let mut conn = establish_connection();
 
     let _res = insert_into(stories::table)
         .values(&story)
         .execute(&mut conn)
-        .unwrap();
+        .map_err(internal_error)?;
 
-    http::StatusCode::CREATED
+    Ok(http::StatusCode::CREATED)
 }
 
-pub async fn publish_story(Path(id_selector): Path<i32>) -> impl IntoResponse {
+pub async fn publish_story(Path(id_selector): Path<i32>) -> Response {
     let mut conn = establish_connection();
 
-    let _res = update(stories::table.find(id_selector))
+    let num_rows = update(stories::table.find(id_selector))
         .set(is_published.eq(true))
         .execute(&mut conn)
-        .unwrap();
+        .map_err(internal_error)?;
 
-    http::StatusCode::NO_CONTENT
+    if let 0 = num_rows {
+        return Ok(http::StatusCode::NOT_FOUND);
+    }
+
+    Ok(http::StatusCode::NO_CONTENT)
 }
 
-pub async fn delete_story(Path(id_selector): Path<i32>) -> impl IntoResponse {
+pub async fn delete_story(Path(id_selector): Path<i32>) -> Response {
     let mut conn = establish_connection();
 
-    let _res = delete(stories::table.find(id_selector))
+    let num_rows = delete(stories::table.find(id_selector))
         .execute(&mut conn)
-        .unwrap();
+        .map_err(internal_error)?;
 
-    http::StatusCode::NO_CONTENT
+    if let 0 = num_rows {
+        return Ok(http::StatusCode::NOT_FOUND);
+    }
+
+    Ok(http::StatusCode::NO_CONTENT)
 }
