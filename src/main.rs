a611e2b72ad72fb99a0b547b4b5b13384ffec4d2
stanislav-az <st.zhdanovich@gmail.com>
2023-05-08 14:26:29 +0700

    add tower http logs

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,29 @@
 use axum::http;
 use axum::routing;
+use tower_http::trace::DefaultMakeSpan;
+use tower_http::trace::DefaultOnFailure;
+use tower_http::trace::DefaultOnRequest;
+use tower_http::trace::DefaultOnResponse;
+use tower_http::trace::TraceLayer;
+use tracing::Level;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+use dotenv::dotenv;
+
 use news_rest_rs::news::handlers;
 
 #[tokio::main]
 async fn main() {
+    dotenv().ok();
+
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| {
+                "news_rest_rs=trace,tower_http=trace,axum::rejection=trace".into()
+            }),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
     let app = axum::Router::new()
         .fallback(fallback_handler)
         .route("/api/tags/:id", routing::delete(handlers::delete_tag))
@@ -21,7 +41,14 @@ async fn main() {
         .route("/api/stories/:id", routing::delete(handlers::delete_story).get(handlers::get_story).patch(handlers::update_story))
         .route("/api/stories", routing::post(handlers::create_story))
         .route("/api/stories", routing::get(handlers::get_stories))
-        .route("/", routing::get(|| async { "Hello, World!" }));
+        .route("/", routing::get(|| async { "Hello, World!" }))
+        .layer(
+            TraceLayer::new_for_http()
+                .make_span_with(DefaultMakeSpan::new().include_headers(true))
+                .on_request(DefaultOnRequest::new().level(Level::INFO))
+                .on_response(DefaultOnResponse::new().level(Level::INFO))
+                .on_failure(DefaultOnFailure::new().level(Level::WARN)),
+        );
 
     // Run our application as a hyper server on http://localhost:3000.
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
